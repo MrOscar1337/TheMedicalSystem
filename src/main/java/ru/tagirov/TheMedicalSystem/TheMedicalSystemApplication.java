c06148e9123a4391af4dc3aac3c550eb197f0481
Kamil <tagirov2004@bk.ru>
2025-02-11 15:28:09 +0300

    Initial commit

---
+++ src/main/java/ru/tagirov/TheMedicalSystem/TheMedicalSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.tagirov.TheMedicalSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMedicalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMedicalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tagirov/TheMedicalSystem/TheMedicalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tagirov.TheMedicalSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMedicalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
